1fbc492ba28025d34f79aecfa8760caa6a444a6c
bdbrink <brendan.brink@outlook.com>
2024-07-09 17:45:23 -0500

    get data type, move logic to seperate function

--- heaps.py
+++ heaps.py
@@ -11,27 +11,31 @@ def heap():
     
     return heap
 
+def heap_parsing(heap):
+    
+    print("Heap after pushing elements:", heap)
+
+    # Popping the smallest element
+    smallest = heapq.heappop(heap)
+    print("Smallest element (popped):", smallest)
+    print("Heap after popping:", heap)
+
+    # Peeking at the smallest element without removing it
+    print("Smallest element (peeked):", heap[0])
+
+    # Creating a heap from a list
+    numbers = [5, 3, 7, 1, 9, 2]
+    heapq.heapify(numbers)
+    print("Heapified list:", numbers)
+
+    # Getting the n smallest elements
+    n_smallest = heapq.nsmallest(3, numbers)
+    print("3 smallest elements:", n_smallest)
+
+    # Getting the n largest elements
+    n_largest = heapq.nlargest(2, numbers)
+    print("2 largest elements:", n_largest)
 
 heap = heap()
-print("Heap after pushing elements:", heap)
-
-# Popping the smallest element
-smallest = heapq.heappop(heap)
-print("Smallest element (popped):", smallest)
-print("Heap after popping:", heap)
-
-# Peeking at the smallest element without removing it
-print("Smallest element (peeked):", heap[0])
-
-# Creating a heap from a list
-numbers = [5, 3, 7, 1, 9, 2]
-heapq.heapify(numbers)
-print("Heapified list:", numbers)
-
-# Getting the n smallest elements
-n_smallest = heapq.nsmallest(3, numbers)
-print("3 smallest elements:", n_smallest)
-
-# Getting the n largest elements
-n_largest = heapq.nlargest(2, numbers)
-print("2 largest elements:", n_largest)
+print(type(heap))
+heap_parsing(heap)
