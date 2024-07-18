e661ff266a1f22ac82861fa3abc49ff7ece856c0
bdbrink <brendan.brink@outlook.com>
2024-07-17 19:37:47 -0500

    adding int to roman numeral

--- quiz/roman_numeral.py
+++ quiz/roman_numeral.py
@@ -24,3 +24,21 @@ class Solution:
             prev_value = current_value
         
         return total
+    
+
+class SolutioInt:
+    def intToRoman(self, num: int) -> str:
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        
+        roman = ""
+        
+        for i, value in enumerate(values):
+            # While the number is greater than or equal to the current value
+            while num >= value:
+                # Add the corresponding symbol to the result
+                roman += symbols[i]
+                # Subtract the value from the number
+                num -= value
+        
+        return roman
